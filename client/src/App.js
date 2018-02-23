43943c5df37c611d9f0de4f94d2c1a7cf34d3254
Joshua Wootonn <joshuawootonn@gmail.com>
2018-02-23 06:26:57 -0600

    reset react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Auth from './auth/auth';
+
 
 class App extends Component {
-  componentDidMount(){
-    const auth = new Auth();
-    auth.login();
-  }
+  
   render() {
+    const { isAuthenticated } = Auth;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        
       </div>
     );
   }
