8488f7d9079f90b7d786921d77b0d87e2cfd7f8d
Joshua Wootonn <joshuawootonn@gmail.com>
2018-02-25 11:16:56 -0600

    Finished the get/post request controller for files

--- server/controller/upload.controller.js
+++ server/controller/upload.controller.js
@@ -1,30 +1,43 @@
 const fs = require("fs");
+const  path = require('path');
+const SAVE_FOLDER = path.join(`${__dirname}`,`..`,`saves`);
+
 exports.create = function(req, res) {
   if (!req.params.sessionId) {
     res.status(400).send({ message: "Need a valid key for your session" });
   }
   if (!req.files) return res.status(400).send("No files were uploaded.");
-  // Gets the file object of the req
+  // Gets the file object of the req 
   let file = req.files.file;
   // Create dir if doesn't already exist
-  if (!fs.existsSync(`saves/${req.params.sessionId}`)) {
-    fs.mkdirSync(`saves/${req.params.sessionId}`);
+  if (!fs.existsSync(`${SAVE_FOLDER}/${req.params.sessionId}`)) {
+    fs.mkdirSync(`${SAVE_FOLDER}/${req.params.sessionId}`);
   }
   // Moves files onto the saves/sessionId/ folder
-  file.mv(`saves/${req.params.sessionId}/${req.files.file.name}`, function(err) {
+  file.mv(`${SAVE_FOLDER}/${req.params.sessionId}/${file.name}`, function(err) {
     if (err) return res.status(500).send(err);
     res.send("File uploaded!");
   });
 };
 
 exports.findOne = function(req, res) {
-  Session.findById(req.params.sessionId, function(err, session) {
-    if (err) {
-      res.status(500).send({ message: "Session retrieval error" });
-    } else {
-      res.send(session);
-    }
-  });
+  if (!req.params.sessionId) {
+    res.status(400).send({ message: "Need a valid key for your session" });
+  }
+  if (!fs.existsSync(`${SAVE_FOLDER}/${req.params.sessionId}`)) {
+    res.status(400).send({message: "No files found for given session"})
+  }
+  // Gets the dir of this user
+  const currentPath =  path.join(`${SAVE_FOLDER}/${req.params.sessionId}`)
+  // Gets the files for that dir
+  const files = fs.readdirSync(currentPath)
+
+  if(!files.length){
+    res.status(400).send({message: "No files saved for this sessiono"});
+  }
+  res.sendFile(currentPath+"/"+ files[0]);
+
+  
 };
 
 exports.update = function(req, res) {
