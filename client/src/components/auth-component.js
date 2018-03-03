d996e133a8e2ab45273280089a3cbccc4bee5002
Joshua Wootonn <joshuawootonn@gmail.com>
2018-03-03 02:03:08 -0600

    Created tab scss for the signup/signin tabs

--- client/src/components/auth-component.js
+++ client/src/components/auth-component.js
@@ -4,12 +4,16 @@ import SignUpContainer from "../containers/auth/signup-container";
 export const AuthComponent = props => {
   return (
     <div className="container center hero height-100">
-      <div>
-        <h1>Filedropper</h1>
-        <button onClick={props.setFormSignIn}>SignIn</button>
-        <button onClick={props.setFormSignUp}>SignUP</button>
-        {props.formType ? <SignInContainer /> : <SignUpContainer />}
-      </div>
+   
+        <div className="row">
+          <div className="six columns tab">
+            {" "}<h1>Filedropper</h1>
+            <button onClick={props.setFormSignIn}>SignIn</button>
+            <button onClick={props.setFormSignUp}>SignUP</button>
+            {props.formType ? <SignInContainer /> : <SignUpContainer />}
+          </div>
+        </div>
+     
     </div>
   );
 };
