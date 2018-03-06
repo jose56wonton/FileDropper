1602968c25b84e419833379c412732ddb84006f7
Joshua Wootonn <joshuawootonn@gmail.com>
2018-03-06 00:01:31 -0600

    Navbar half done!!

--- client/src/containers/general/nav-container.js
+++ client/src/containers/general/nav-container.js
@@ -1,17 +1,59 @@
 import React, { Component } from "react";
-
+import * as actions from "../../actions";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
 class NavContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formType: true
+    };
+  }
+  home = () => {
+    
+  }
+  logout = () => {
+    
+  }
+  login = () => {
+    
+  }
+  user = () => {
+    
+  }
   render() {
+    const auth = this.props.user.value;
+    let right = null ;
+    if(this.props.user.value){
+      right = <div className="right menu">
+      <a className="ui item">Logout</a>
+      <a className="item"><i className="user icon" /></a>
+    </div>
+    }
+    else{
+      right = <div className="right menu">
+       <a className="ui item">Login</a>}
+    </div>
+    }
     return (
       <div className="container">
-        <ul className="navbar-list">
-          <li className="navbar-item">asdf</li>
-          <li className="navbar-item">asdf</li>
-          <li className="navbar-item">asdf</li>
-        </ul>
+        <div className="ui secondary pointing menu">
+          <a className="active item">Home</a>
+         
+          {right}
+          
+          
+        </div>
       </div>
     );
   }
 }
 
-export default NavContainer;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.user
+  };
+};
+
+
+export default connect(mapStateToProps, actions)(withRouter(NavContainer)) ;
